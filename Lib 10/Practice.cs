98cfdf43bcd1b559028960facdeeccce3929e7e6
darkd6002@outlook.com <darkd6002@outlook.com>
2021-12-26 14:39:13 +0300

    Добавьте файлы проекта.

---
+++ Lib 10/Practice.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib_10
+{
+    public class Practice
+    {
+        public static (int sum, string number, int count) Total(int k)
+        {
+            Random rnd = new Random();
+            int count = 0;
+            int sum = 0;
+            int num = 0;
+            string number = string.Empty;
+            while (sum+num < k)
+            {
+                num = rnd.Next(5, 10);
+                sum += num;
+                count++;
+                number += num.ToString() + " ";
+            }
+            var result = (sum , number, count);
+            return result;
+        }
+    }
+}
---
+++ practice 1/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Lib_10;
+
+namespace practice_1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Calculate_Click(object sender, RoutedEventArgs e)
+        {
+            int k;
+            bool f = Int32.TryParse(inputValue.Text, out k);
+            if (f == true)
+            {
+                var result = Practice.Total(k);
+                Summ.Text = result.sum.ToString();
+                Nambers.Text = result.number;
+                CountNumbers.Text = result.count.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Ведите корректные данные!");
+            }
+        }
+
+        private void Info_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Харенко Кирилл ИСП-34", "Разработчик", MessageBoxButton.OK);
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
